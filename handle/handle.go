110390f144103308b5b8ebc2c4c1c27e4fea233e
Temur Yusupov <yusupov.temur@gmail.com>
2018-09-05 11:13:48 +0500

    refactor

--- handle/handle.go
+++ handle/handle.go
@@ -6,9 +6,10 @@ import (
 	"log"
 	"net/http"
 
-	account "github.com/tusupov/go-exercises/bank-account"
+	"github.com/tusupov/go-exercises/bank-account"
 )
 
+// Error codes returned by failures to parse an expression.
 var (
 	errCreated     = errors.New("Account is created")
 	errFailCreated = errors.New("Failed to create account, a negative initial deposit")
@@ -26,11 +27,11 @@ func New() *handle {
 	return &handle{}
 }
 
-// AccountOpen
+// AccountOpen handle function
 func (h *handle) AccountOpen(w http.ResponseWriter, r *http.Request) {
 	// Account opened
 	if h.account != nil {
-		JSONErrorResponse(w, http.StatusBadRequest, errCreated)
+		JSONErrorResponse(w, http.StatusBadRequest, errCreated.Error())
 		return
 	}
 
@@ -38,47 +39,49 @@ func (h *handle) AccountOpen(w http.ResponseWriter, r *http.Request) {
 	var accountRequest account.Request
 	err := json.NewDecoder(r.Body).Decode(&accountRequest)
 	if err != nil {
-		JSONErrorResponse(w, http.StatusInternalServerError, err)
+		JSONErrorResponse(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Open account
 	h.account = account.Open(accountRequest.Balance)
 	if h.account == nil {
-		JSONErrorResponse(w, http.StatusBadRequest, errFailCreated)
+		JSONErrorResponse(w, http.StatusBadRequest, errFailCreated.Error())
 		return
 	}
 
 	JSONSuccessResponse(w, account.Response{Balance: accountRequest.Balance})
 }
 
+// AccountBalance handle function
 func (h *handle) AccountBalance(w http.ResponseWriter, r *http.Request) {
 	// Account not created
 	if h.account == nil {
-		JSONErrorResponse(w, http.StatusBadRequest, errNotCreated)
+		JSONErrorResponse(w, http.StatusBadRequest, errNotCreated.Error())
 		return
 	}
 
 	// Get account balance
 	balance, ok := h.account.Balance()
 	if !ok {
-		JSONErrorResponse(w, http.StatusBadRequest, errClosed)
+		JSONErrorResponse(w, http.StatusBadRequest, errClosed.Error())
 		return
 	}
 
 	JSONSuccessResponse(w, account.Response{Balance: balance})
 }
 
+// AccountDeposit handle function
 func (h *handle) AccountDeposit(w http.ResponseWriter, r *http.Request) {
 	// Account not created
 	if h.account == nil {
-		JSONErrorResponse(w, http.StatusBadRequest, errNotCreated)
+		JSONErrorResponse(w, http.StatusBadRequest, errNotCreated.Error())
 		return
 	}
 
 	// Check to closed
 	if _, ok := h.account.Balance(); !ok {
-		JSONErrorResponse(w, http.StatusBadRequest, errClosed)
+		JSONErrorResponse(w, http.StatusBadRequest, errClosed.Error())
 		return
 	}
 
@@ -86,50 +89,54 @@ func (h *handle) AccountDeposit(w http.ResponseWriter, r *http.Request) {
 	var accountResponse account.Response
 	err := json.NewDecoder(r.Body).Decode(&accountResponse)
 	if err != nil {
-		JSONErrorResponse(w, http.StatusInternalServerError, err)
+		JSONErrorResponse(w, http.StatusInternalServerError, err.Error())
 		return
 	}
 
 	// Deposit
 	newBalance, ok := h.account.Deposit(accountResponse.Balance)
 	if !ok {
-		JSONErrorResponse(w, http.StatusBadRequest, errNotEnough)
+		JSONErrorResponse(w, http.StatusBadRequest, errNotEnough.Error())
 		return
 	}
 
 	JSONSuccessResponse(w, account.Response{Balance: newBalance})
 }
 
+// AccountClose handle function
 func (h *handle) AccountClose(w http.ResponseWriter, r *http.Request) {
 	// Account not created
 	if h.account == nil {
-		JSONErrorResponse(w, http.StatusBadRequest, errNotCreated)
+		JSONErrorResponse(w, http.StatusBadRequest, errNotCreated.Error())
 		return
 	}
 
 	// Close account
 	payout, ok := h.account.Close()
 	if !ok {
-		JSONErrorResponse(w, http.StatusBadRequest, errClosed)
+		JSONErrorResponse(w, http.StatusBadRequest, errClosed.Error())
 		return
 	}
 
 	JSONSuccessResponse(w, account.Response{Balance: payout})
 }
 
-func JSONSuccessResponse(w http.ResponseWriter, result interface{}) {
-	JSONResponse(w, http.StatusOK, result)
+func JSONSuccessResponse(w http.ResponseWriter, resp interface{}) {
+	JSONResponse(w, http.StatusOK, resp)
 }
 
-func JSONErrorResponse(w http.ResponseWriter, errorCode int, err error) {
+// Write
+func JSONErrorResponse(w http.ResponseWriter, errorCode int, err string) {
 	type ApiError struct {
 		Error string `json:"error"`
 	}
 	JSONResponse(w, errorCode, ApiError{
-		Error: err.Error(),
+		Error: err,
 	})
 }
 
+// Write JSON data to http writer
+// Encode resp to json and write to w
 func JSONResponse(w http.ResponseWriter, statusCode int, resp interface{}) {
 
 	respJson, err := json.MarshalIndent(resp, "", "    ")
